89d8bde854bb4b4e0773c5aff74dd143975ccb4f
Rohit Telkar <115656028+Rohit05082001@users.noreply.github.com>
2024-04-12 17:32:13 +0000

    Configure

---
+++ Day_01/test.js
@@ -0,0 +1,23 @@
+console.log("Hello Rohit")
+
+// var is used to declear a variable in javascript.
+// variable decleared using var are able to redeclear and it is accessable throughout the code.
+// we can also able to change its value based upon reqirement.
+var a = 10;
+console.log(a) // Expected output 10
+var a = "Rohit" 
+console.log(a); // Expected output Rohit
+
+// let is used to declear a variable in javascript.
+// variable decleared using let are unbable to redeclear and it is accessable within block {...*____*...}
+// we can also able to change its value based upon reqirement
+
+let x = 10;
+let b = 20;
+console.log(x+b) // Expected output is 30
+
+// const is also used to declear a variable in javascript
+// variable decleared using const are unable to redeclear and it is accessable within block of code {...*___*...}
+// we are unable to change the value of its variable. once it assign value it is final value we can not modified it
+const pi = 3.14
+console.log(pi) // Expected output is 3.14
